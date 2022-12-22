4524295a2fe8834bddd55d171f692ae365dfc9e6
besimds <121216245+besimds@users.noreply.github.com>
2022-12-22 07:36:41 +0100

    Sm
    Update

---
+++ ReadLater5/ReadLater5/Controllers/BookmarkAPIController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Entity;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Services;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace ReadLater5.Controllers
+{
+    [Authorize]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookmarkAPIController : ControllerBase
+    {
+
+
+
+
+
+        IBookmarkAPIService _bookmarkAPIService;
+
+        public BookmarkAPIController(IBookmarkAPIService bookmarkAPIService)
+        {
+            
+            _bookmarkAPIService = bookmarkAPIService;
+
+            
+        }
+
+        // GET: api/BookmarkAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bookmark>>> GetBookmark()
+        {
+            return await _bookmarkAPIService.GetBookmarks();
+        }
+
+        // GET: api/BookmarkAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bookmark>> GetBookmark(int id)
+        {
+            return await _bookmarkAPIService.GetBookmark(id);
+        }
+
+        // PUT: api/BookmarkAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBookmark(int id, Bookmark bookmark)
+        {
+
+            return (IActionResult)_bookmarkAPIService.PutBookmark(id, bookmark);
+        }
+
+        // POST: api/BookmarkAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Bookmark>> PostBookmark(Bookmark bookmark)
+        {
+            //_context.Bookmark.Add(bookmark);
+            //await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetBookmark", new { id = bookmark.ID }, bookmark);
+
+            return _bookmarkAPIService.PostBookmark(bookmark);
+        }
+
+        // DELETE: api/BookmarkAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookmark(int id)
+        {
+            return (IActionResult)_bookmarkAPIService.DeleteBookmarkAsync(id);
+        }
+
+    }
+}
---
+++ ReadLater5/ReadLater5/Controllers/BookmarksController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Entity;
+using Microsoft.AspNetCore.Authorization;
+using Services;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace ReadLater5.Controllers
+{
+    [Authorize]
+    public class BookmarksController : Controller
+    {
+        
+        IBookmarkService _bookmarkService;
+
+        public BookmarksController(IBookmarkService bookmarkService, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _bookmarkService = bookmarkService;
+        }
+
+        // GET: Bookmarks
+        public async Task<IActionResult> Index()
+        {
+            //var readLaterDataContext = _context.Bookmark.Include(b => b.Category);
+            //return View(await readLaterDataContext.ToListAsync());
+            List<Bookmark> model = await _bookmarkService.GetBookmarks();
+            return View(model);
+        }
+
+        // GET: Bookmarks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark= await _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewData["CategoryId"] = new SelectList(await _bookmarkService.selectListCategories(), "ID", "Name");
+            //return View();
+            return View();
+        }
+
+        // POST: Bookmarks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,URL,ShortDescription,CategoryId,CreateDate")] Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookmarkService.CreateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+            ViewData["CategoryId"] = new SelectList(await _bookmarkService.selectListCategories(), "ID", "Name", bookmark.CategoryId);
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = await _bookmarkService.GetBookmark((int)id);
+            if (bookmark== null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            ViewData["CategoryId"] = new SelectList(await _bookmarkService.selectListCategories(), "ID", "Name", bookmark.CategoryId);
+            return View(bookmark);
+        }
+
+        // POST: Bookmarks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,URL,ShortDescription,CategoryId,CreateDate")] Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookmarkService.UpdateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+            ViewData["CategoryId"] = new SelectList(await _bookmarkService.selectListCategories(), "ID", "Name", bookmark.CategoryId);
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark= await _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // POST: Bookmarks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Bookmark bookmark = await _bookmarkService.GetBookmark(id);
+            _bookmarkService.DeleteBookmark(bookmark);
+            return RedirectToAction("Index");
+        }
+
+        private bool BookmarkExists(int id)
+        {
+            return _bookmarkService.GetBookmark((int)id) == null ? false : true;
+        }
+    }
+}
